0298652f27b34f96bd4965f60d0d745a3154aabf
Dave Bilotta <davebilotta@gmail.com>
2013-12-04 20:01:02 -0500

    More

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Lifenotes::Application.config.secret_key_base = 'ebd95f2466305b50ed78a8c350d899a66f63bcd7b239d2406d3ca395e052d050a114b3ae4d5f2307d4eed24fd1e6c69471f4d6894dccbd0a3fe9b163c008193b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Lifenotes::Application.config.session_store :cookie_store, key: '_lifenotes_session'
